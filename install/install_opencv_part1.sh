0da30b86943981cca8d7f890b1efa360d3ab84bd
Alex Tolnai <ajt1996@gmail.com>
2020-03-27 16:35:47 -0500

    Update install_opencv_part1.sh

--- install/install_opencv_part1.sh
+++ install/install_opencv_part1.sh
@@ -14,6 +14,10 @@ sudo apt-get install libxvidcore-dev libx264-dev -y
 sudo apt install python-numpy python3-numpy -y
 sudo apt install libgtk-3-dev libcanberra-gtk3-dev -y
 
+#Used for OpenVino install
+sudo usermod -a -G users "$(whoami)"
+
 #add to system path
 cd ~
 echo 'export LD_LIBRARY_PATH=/opt/opencv-4.1.0/lib:$LD_LIBRARY_PATH' >> .bashrc
+. .bashrc
