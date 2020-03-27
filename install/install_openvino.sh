eb56a9dc8edad49038cab4dbb34817e707310cef
s3540645 <s3540645@student.rmit.edu.au.com>
2020-03-26 20:19:04 -0500

    fixed opencv install tar and image pathway download

--- install/install_openvino.sh
+++ install/install_openvino.sh
@@ -1,5 +1,5 @@
 #Install the OpenVINO™ Toolkit for Raspbian* OS Package
-mkdir ~/OpenVINO
+mkdir -p ~/OpenVINO/datasets/test
 cd ~/OpenVINO/
 wget https://download.01.org/opencv/2019/openvinotoolkit/R3/l_openvino_toolkit_runtime_raspbian_p_2019.3.334.tgz
 sudo mkdir -p /opt/intel/openvino
@@ -23,7 +23,7 @@ cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-march=armv7-a" /opt/intel/o
 make -j2 object_detection_sample_ssd
 wget --no-check-certificate https://download.01.org/opencv/2019/open_model_zoo/R1/models_bin/face-detection-adas-0001/FP16/face-detection-adas-0001.bin
 wget --no-check-certificate https://download.01.org/opencv/2019/open_model_zoo/R1/models_bin/face-detection-adas-0001/FP16/face-detection-adas-0001.xml
-wget https://upload.wikimedia.org/wikipedia/commons/e/eb/T_Roosevelt.jpg -O ~/OpenVINO/datasets/test
+wget https://upload.wikimedia.org/wikipedia/commons/e/eb/T_Roosevelt.jpg -P ~/OpenVINO/datasets/test
 ./armv7l/Release/object_detection_sample_ssd -m face-detection-adas-0001.xml -d MYRIAD -i ~/OpenVINO/datasets/test/T_Roosevelt.jpg
 
 
