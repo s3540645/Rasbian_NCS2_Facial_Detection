292b70ce6593634f54fa6ba96a24e2296c1751a2
Alex Tolnai <ajt1996@gmail.com>
2020-03-27 16:21:38 -0500

    Update install_all_part2.sh

--- install/install_all_part2.sh
+++ install/install_all_part2.sh
@@ -4,7 +4,7 @@ chmod +x install_opencv_part2.sh
 chmod +x install_openvino.sh
 chmod +x download_models.sh
 
-sh install_opencv_part2.sh
-sh install_openvino.sh
-sh download_models.sh
+./install_opencv_part2.sh
+./install_openvino.sh
+./download_models.sh
 cd ~/OpenVINO/Raspbian_NCS2_Facial_Detection
