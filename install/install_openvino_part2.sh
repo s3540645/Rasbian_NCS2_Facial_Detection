833817c6e352799a92a767387cf1e438fa731549
Alex Tolnai <ajt1996@gmail.com>
2020-03-28 15:04:53 -0500

    Update install_openvino_part2.sh

--- install/install_openvino_part2.sh
+++ install/install_openvino_part2.sh
@@ -5,41 +5,6 @@ echo "install_OpenVino_part2 start"
 echo "================================"
 
 echo "--------------------------------"
-echo "download OpenVino and setup"
-echo "--------------------------------"
-
-#download OpenVino and setup
-mkdir -p ~/OpenVINO/datasets/test
-cd ~/OpenVINO/
-wget https://download.01.org/opencv/2019/openvinotoolkit/R3/l_openvino_toolkit_runtime_raspbian_p_2019.3.334.tgz
-sudo mkdir -p /opt/intel/openvino
-sudo tar -xf  l_openvino_toolkit_runtime_raspbian_p_2019.3.334.tgz --strip 1 -C /opt/intel/openvino
-rm -rf l_openvino_toolkit_runtime_raspbian_p_2019.3.334.tgz
-
-echo "--------------------------------"
-echo "Install External Software Dependencies"
-echo "--------------------------------"
-
-#Install External Software Dependencies
-sudo apt install cmake  -y
-
-echo "--------------------------------"
-echo "Set the Environment Variables"
-echo "--------------------------------"
-
-#Set the Environment Variables
-source /opt/intel/openvino/bin/setupvars.sh
-
-echo "--------------------------------"
-echo "Add USB Rules"
-echo "--------------------------------"
-
-#Add USB Rules
-cd /opt/intel/openvino/install_dependencies/
-sudo chmod +x install_NCS_udev_rules.sh
-./install_NCS_udev_rules.sh
-
-echo "--------------------------------"
 echo "Build and Run Object Detection Sample"
 echo "--------------------------------"
 
