4b2e79df4377a2d4076463b807262a8510b6c84d
Alex Tolnai <ajt1996@gmail.com>
2020-03-28 14:59:02 -0500

    Update install_openvino_part2.sh

--- install/install_openvino_part2.sh
+++ install/install_openvino_part2.sh
@@ -44,8 +44,8 @@ echo "Build and Run Object Detection Sample"
 echo "--------------------------------"
 
 #Build and Run Object Detection 
-cd ~/OpenVINO/Sample
-mkdir build && cd build
+mkdir -p ~/OpenVINO/build
+cd ~/OpenVINO/build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-march=armv7-a" /opt/intel/openvino/deployment_tools/inference_engine/samples
 make -j2 object_detection_sample_ssd
 wget --no-check-certificate https://download.01.org/opencv/2019/open_model_zoo/R1/models_bin/face-detection-adas-0001/FP16/face-detection-adas-0001.bin
