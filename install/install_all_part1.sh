549f07e60fa76937fe8e5fb7fda14fea69f58a66
Alex Tolnai <ajt1996@gmail.com>
2020-03-28 15:07:32 -0500

    Update install_all_part1.sh

--- install/install_all_part1.sh
+++ install/install_all_part1.sh
@@ -4,8 +4,6 @@ echo "================================"
 echo "install_all_part1 start"
 echo "================================"
 
-chmod +x install_opencv_part1.sh
-
 ./install_opencv_part1.sh
 
 echo "================================"
