0cafedd675da4f0ea7cb1c89848b1ee0731358ae
Alex Tolnai <ajt1996@gmail.com>
2020-03-28 15:07:59 -0500

    Update install_all_part2.sh

--- install/install_all_part2.sh
+++ install/install_all_part2.sh
@@ -5,13 +5,6 @@ echo "install_all_part2 start"
 echo "================================"
 
 echo "--------------------------------"
-echo "chmod for sh"
-echo "--------------------------------"
-
-chmod +x install_opencv_part2.sh
-chmod +x install_openvino_part1.sh
-
-echo "--------------------------------"
 echo "run opencv part 2, openvino part 1"
 echo "--------------------------------"
 
